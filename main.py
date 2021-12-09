76d3f61de1cc85106e6aedb2bc9705fd5a814ce5
DH711-creator <83188352+DH711-creator@users.noreply.github.com>
2021-12-08 21:40:05 -0500

    Create basic commands
    getting the bot up and running, didn't feel like figuring out how to commit from vscode so just copied and pasted it here... I'll do that tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 import openai
 import discord
+from discord.ext import commands
+import os
 
-#do things
+BOTSTATUS="under construction"
+
+bot = commands.Bot(command_prefix='ai.')
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    bot.change_presence(activity=discord.Game(name='in development'), status=discord.Status.do_not_disturb)
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title='Help', description='List of commands', color=0x00ff00)
+    embed.add_field(name='ai.help', value='Shows this message', inline=False)
+    embed.add_field(name='ai.status', value='Shows the status of the AI', inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def status(ctx):
+    embed = discord.Embed(title='Status', description='Current status of the AI', color=0xffa500)
+    embed.add_field(name='Status', value=f"Currently I am {BOTSTATUS}", inline=False)
+    await ctx.send(embed=embed)
+
+bot.run('DISCORD_KEY')
+
+
