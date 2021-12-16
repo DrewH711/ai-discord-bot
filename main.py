20dcde3000b005af2ab95f26063844f529f7d77b
unknown <drexh47127@gmail.com>
2021-12-15 19:53:24 -0500

    Added more basic functionalities and keys--hopefully those won't be shown publicly. Guess we'll find out!

--- main.py
+++ main.py
@@ -1,4 +1,54 @@
 import openai
+from openai import error
+error.APIError.default_message = 'Error: {0}'
 import discord
+from discord.ext import commands
+import os
+from os import getenv
+from dotenv import load_dotenv
 
-#do things
+load_dotenv("C:/Users/holla/Documents/aibot/ai-discord-bot-1/keys.env")
+
+BOTSTATUS="under construction"
+
+bot = commands.Bot(command_prefix='ai.')
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_presence(activity=discord.Game(name='in development'), status=discord.Status.do_not_disturb)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Responded in {bot.latency * 1000:.0f}ms')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title='Help', description='List of commands', color=0x00ff00)
+    embed.add_field(name='ai.help', value='Shows this message', inline=False)
+    embed.add_field(name='ai.status', value='Shows the status of the AI', inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def status(ctx):
+    embed = discord.Embed(title='Status', description=f"Currently I am {BOTSTATUS}", color=0xffa500)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def request(ctx, engine):
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    x = openai.Engine.retrieve(f"{engine}")
+    try:
+        await ctx.send(f"{x.id} is available: {x.ready}")
+    except error.APIError:
+        await ctx.send(f"{engine} is not a valid engine")
+    
+
+bot.run(os.getenv('DISCORD_TOKEN'))
+
+
