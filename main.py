48b794e1f45f5e30471b92c47d5cdefbb6520f52
DrewH711-creator <drexh47127@gmail.com>
2021-12-15 20:40:07 -0500

    Added a command to explain code with ai
    Needs lots of refining but beginning work on basic functionalities

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import openai
 from openai import error
-error.APIError.default_message = 'Error: {0}'
 import discord
 from discord.ext import commands
 import os
@@ -8,7 +7,6 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv("C:/Users/holla/Documents/aibot/ai-discord-bot-1/keys.env")
-
 BOTSTATUS="under construction"
 
 bot = commands.Bot(command_prefix='ai.')
@@ -43,11 +41,25 @@ async def request(ctx, engine):
     openai.api_key = os.getenv('OPENAI_KEY')
 
     x = openai.Engine.retrieve(f"{engine}")
+    #if anyone knows how to actually make this handle the exception properly, please let me know
+    #I've tried all kinds of variations of "openai.error.APIError" but it still doesn't work 
     try:
         await ctx.send(f"{x.id} is available: {x.ready}")
     except error.APIError:
         await ctx.send(f"{engine} is not a valid engine")
-    
+
+@bot.command()
+async def explaincode(ctx, language, *, code):
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    response=openai.Completion.create(
+    engine="davinci-codex",
+    prompt=f"explain the following {language} code: \n{code}",
+    max_tokens=1000,
+    temperature=0,
+    )
+    print(response)
+    await ctx.send(f"```{language}\n{response.choices[0].text}```")
 
 bot.run(os.getenv('DISCORD_TOKEN'))
 
