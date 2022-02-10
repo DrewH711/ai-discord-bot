5a06fbbd06b29cc4b63ebfd474eb6374826d8a25
Maddox Nichols <mnichols@themaddoxnetwork.com>
2022-02-09 20:32:15 -0500

    Re-enable Web Server

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord_ui import UI
 from dotenv import load_dotenv
 import os
+import WebServer
 cooldown = []
 load_dotenv("aibot/keys.env")
 intents=Intents.default()
@@ -24,4 +25,5 @@ bot.add_cog(codeCommands.codeCommands(bot,cooldown))
 import regularCommands
 bot.add_cog(regularCommands.regularCommands(bot,cooldown))
 
+WebServer.start()
 bot.run(os.getenv("DISCORD_TOKEN"))
